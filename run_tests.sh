f4a477ee1d567b55a300b27e5a0d8a30ff6539b7
Timothy Middlemas <tm17@princeton.edu>
2020-05-21 23:23:55 -0400

    Modified run_tests.sh, added block averaging, and added chisq script

--- run_tests.sh
+++ run_tests.sh
@@ -20,11 +20,11 @@ done
 
 ../compute_nn -c 3.0 -n 30 -s 100000 --cycles 100 2D_square_lattice/data/config{0..9}.dat > 2D_square_lattice/30bin.dat
 ../compute_nn -c 3.0 -n 120 -s 100000 --cycles 100 2D_square_lattice/data/config{0..9}.dat > 2D_square_lattice/120bin.dat
-../compute_nn -c 3.0 -n 480 -s 100000 --cycles 100 2D_square_lattice/data/config{0..9}.dat > 2D_square_lattice/480bin.dat
-../compute_nn -c 3.0 -n 1920 -s 100000 --cycles 100 2D_square_lattice/data/config{0..9}.dat > 2D_square_lattice/1920bin.dat
+../compute_nn -c 3.0 -n 480 -s 100000 -b 4 2D_square_lattice/480bin_block.dat --cycles 100 2D_square_lattice/data/config{0..9}.dat > 2D_square_lattice/480bin.dat
+../compute_nn -c 3.0 -n 1920 -s 100000 -b 16 2D_square_lattice/1920bin_block.dat --cycles 100 2D_square_lattice/data/config{0..9}.dat > 2D_square_lattice/1920bin.dat
 
 if [ "$1" = "--full" ]; then
-    ../compute_nn -c 3.0 -n 7680 -s 10000 --cycles 1000 2D_square_lattice/data/config{0..9}.dat > 2D_square_lattice/7680bin.dat
+    ../compute_nn -c 3.0 -n 7680 -s 10000 -b 64 2D_square_lattice/7680bin_block.dat --cycles 1000 2D_square_lattice/data/config{0..9}.dat > 2D_square_lattice/7680bin.dat
 fi
 
 echo "finished 2D square lattice tests"
@@ -38,10 +38,13 @@ for i in {0..999}; do
     ./gen_poisson 1 10000 1D_poisson/data/config$i &> /dev/null
 done
 
-../compute_nn -c 3.0 -n 30 -s 100000 1D_poisson/data/config{0..999}.dat > 1D_poisson/30bin.dat
-../compute_nn -c 3.0 -n 120 -s 100000 1D_poisson/data/config{0..999}.dat > 1D_poisson/120bin.dat
-../compute_nn -c 3.0 -n 480 -s 100000 1D_poisson/data/config{0..999}.dat > 1D_poisson/480bin.dat
-../compute_nn -c 3.0 -n 1920 -s 100000 1D_poisson/data/config{0..999}.dat > 1D_poisson/1920bin.dat
+../compute_nn -c 16.08495438637974 -n 30 -s 100000 1D_poisson/data/config{0..999}.dat > 1D_poisson/30bin.dat
+../compute_nn -c 16.08495438637974 -n 120 -s 100000 -b 4 1D_poisson/120bin_block.dat 1D_poisson/data/config{0..999}.dat > 1D_poisson/120bin.dat
+../compute_nn -c 16.08495438637974 -n 480 -s 100000 -b 16 1D_poisson/480bin_block.dat 1D_poisson/data/config{0..999}.dat > 1D_poisson/480bin.dat
+../compute_nn -c 16.08495438637974 -n 1920 -s 100000 -b 64 1D_poisson/1920bin_block.dat 1D_poisson/data/config{0..999}.dat > 1D_poisson/1920bin.dat
+
+../compute_nn -c 16.08495438637974 -n 480 -s 100000 -b 4 1D_poisson/480bin_block2.dat 1D_poisson/data/config{0..999}.dat > 1D_poisson/480bin2.dat
+../compute_nn -c 16.08495438637974 -n 1920 -s 100000 -b 16 1D_poisson/1920bin_block2.dat 1D_poisson/data/config{0..999}.dat > 1D_poisson/1920bin2.dat
 
 echo "finished 1D poisson tests"
 
@@ -54,10 +57,13 @@ for i in {0..999}; do
     ./gen_poisson 2 10000 2D_poisson/data/config$i &> /dev/null
 done
 
-../compute_nn -c 3.0 -n 30 -s 100000 2D_poisson/data/config{0..999}.dat > 2D_poisson/30bin.dat
-../compute_nn -c 3.0 -n 120 -s 100000 2D_poisson/data/config{0..999}.dat > 2D_poisson/120bin.dat
-../compute_nn -c 3.0 -n 480 -s 100000 2D_poisson/data/config{0..999}.dat > 2D_poisson/480bin.dat
-../compute_nn -c 3.0 -n 1920 -s 100000 2D_poisson/data/config{0..999}.dat > 2D_poisson/1920bin.dat
+../compute_nn -c 4.38178046004133 -n 30 -s 100000 2D_poisson/data/config{0..999}.dat > 2D_poisson/30bin.dat
+../compute_nn -c 4.38178046004133 -n 120 -s 100000 -b 4 2D_poisson/120bin_block.dat 2D_poisson/data/config{0..999}.dat > 2D_poisson/120bin.dat
+../compute_nn -c 4.38178046004133 -n 480 -s 100000 -b 16 2D_poisson/480bin_block.dat 2D_poisson/data/config{0..999}.dat > 2D_poisson/480bin.dat
+../compute_nn -c 4.38178046004133 -n 1920 -s 100000 -b 64 2D_poisson/1920bin_block.dat 2D_poisson/data/config{0..999}.dat > 2D_poisson/1920bin.dat
+
+../compute_nn -c 4.38178046004133 -n 480 -s 100000 -b 4 2D_poisson/480bin_block2.dat 2D_poisson/data/config{0..999}.dat > 2D_poisson/480bin2.dat
+../compute_nn -c 4.38178046004133 -n 1920 -s 100000 -b 16 2D_poisson/1920bin_block2.dat 2D_poisson/data/config{0..999}.dat > 2D_poisson/1920bin2.dat
 
 echo "finished 2D poisson tests"
 
@@ -71,9 +77,12 @@ for i in {0..999}; do
 done
 
 ../compute_nn -c 3.0 -n 30 -s 100000 3D_poisson/data/config{0..999}.dat > 3D_poisson/30bin.dat
-../compute_nn -c 3.0 -n 120 -s 100000 3D_poisson/data/config{0..999}.dat > 3D_poisson/120bin.dat
-../compute_nn -c 3.0 -n 480 -s 100000 3D_poisson/data/config{0..999}.dat > 3D_poisson/480bin.dat
-../compute_nn -c 3.0 -n 1920 -s 100000 3D_poisson/data/config{0..999}.dat > 3D_poisson/1920bin.dat
+../compute_nn -c 3.0 -n 120 -s 100000 -b 4 3D_poisson/120bin_block.dat 3D_poisson/data/config{0..999}.dat > 3D_poisson/120bin.dat
+../compute_nn -c 3.0 -n 480 -s 100000 -b 16 3D_poisson/480bin_block.dat 3D_poisson/data/config{0..999}.dat > 3D_poisson/480bin.dat
+../compute_nn -c 3.0 -n 1920 -s 100000 -b 64 3D_poisson/1920bin_block.dat 3D_poisson/data/config{0..999}.dat > 3D_poisson/1920bin.dat
+
+../compute_nn -c 3.0 -n 480 -s 100000 -b 4 3D_poisson/480bin_block2.dat 3D_poisson/data/config{0..999}.dat > 3D_poisson/480bin2.dat
+../compute_nn -c 3.0 -n 1920 -s 100000 -b 16 3D_poisson/1920bin_block2.dat 3D_poisson/data/config{0..999}.dat > 3D_poisson/1920bin2.dat
 
 echo "finished poisson 3d tests"
 
@@ -88,8 +97,8 @@ done
 
 ../compute_nn -c "1.8e-4" -n 30 -s 100000 1D_RSA/data/config{0..1999}.dat > 1D_RSA/30bin.dat
 ../compute_nn -c "1.8e-4" -n 120 -s 100000 1D_RSA/data/config{0..1999}.dat > 1D_RSA/120bin.dat
-../compute_nn -c "1.8e-4" -n 480 -s 100000 1D_RSA/data/config{0..1999}.dat > 1D_RSA/480bin.dat
-../compute_nn -c "1.8e-4" -n 1920 -s 100000 1D_RSA/data/config{0..1999}.dat > 1D_RSA/1920bin.dat
+../compute_nn -c "1.8e-4" -n 480 -s 100000 -b 4 1D_RSA/480bin_block.dat 1D_RSA/data/config{0..1999}.dat > 1D_RSA/480bin.dat
+../compute_nn -c "1.8e-4" -n 1920 -s 100000 -b 16 1D_RSA/1920bin_block.dat 1D_RSA/data/config{0..1999}.dat > 1D_RSA/1920bin.dat
 
 
 echo "finished 1D RSA tests"
@@ -106,8 +115,8 @@ if [ "$1" == "--full" ]; then
 
     ../compute_nn -c "2.16e-4" -n 30 -s 100000 1D_RSA_long/data/config{0..19999}.dat > 1D_RSA_long/30bin.dat
     ../compute_nn -c "2.16e-4" -n 120 -s 100000 1D_RSA_long/data/config{0..19999}.dat > 1D_RSA_long/120bin.dat
-    ../compute_nn -c "2.16e-4" -n 480 -s 100000 1D_RSA_long/data/config{0..19999}.dat > 1D_RSA_long/480bin.dat
-    ../compute_nn -c "2.16e-4" -n 1920 -s 100000 1D_RSA_long/data/config{0..19999}.dat > 1D_RSA_long/1920bin.dat
+    ../compute_nn -c "2.16e-4" -n 480 -s 100000 -b 4 1D_RSA_long/480bin_block.dat 1D_RSA_long/data/config{0..19999}.dat > 1D_RSA_long/480bin.dat
+    ../compute_nn -c "2.16e-4" -n 1920 -s 100000 -b 16 1D_RSA_long/1920bin_block.dat 1D_RSA_long/data/config{0..19999}.dat > 1D_RSA_long/1920bin.dat
 
     echo "finished long 1D_RSA tests"
 fi
